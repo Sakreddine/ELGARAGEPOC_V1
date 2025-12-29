3eb8eed50ebb034b9c0a2eccda49d0a9f56a5aec
Sakreddine <sakermannai@gmail.com>
2025-12-29 14:22:56 +0100

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import pandas as pd
 #  CONFIGURATION GLOBALE (Mode SaaS Centralisé)
 # ==============================================================================
 ADMIN_SB_URL = "https://ljdzsqpzbxtrptdaftur.supabase.co" # Remplacez par votre URL
-ADMIN_SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # Remplacez par votre Clé
-ADMIN_GROQ_KEY = "gsk_..."                                # Remplacez par votre Clé Groq
+ADMIN_SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZHpzcXB6Ynh0cnB0ZGFmdHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NjA3NTIsImV4cCI6MjA4MjMzNjc1Mn0.Z0IaWz901FG360CrGMHAQBdDJ88md2p2mpCe-Y5yOVY"  # Remplacez par votre Clé
+ADMIN_GROQ_KEY = "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"                                # Remplacez par votre Clé Groq
 
 st.set_page_config(page_title="ELGarage SaaS", layout="wide", initial_sidebar_state="collapsed")
 st.markdown("""<style>.stButton>button { height: 3em; width: 100%; border-radius: 10px; font-weight: bold; } .report-container { background-color: #f8f9fa; border: 2px solid #f25c05; border-radius: 10px; padding: 15px; margin-bottom: 20px; } #MainMenu {visibility: hidden;} footer {visibility: hidden;} .block-container { padding-top: 2rem; }</style>""", unsafe_allow_html=True)
@@ -206,3 +206,4 @@ else:
                         if ai:
                             r = ai.check_maintenance_schedule(v_info, dm.get_full_history_text(v_id))
                             if "error" not in r: st.markdown(r['response']); dm.save_echeance(v_id, r['response'])
+
